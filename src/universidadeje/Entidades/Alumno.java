cb1a2a181227ed6bef46986d19187a23932546ed
Gabriel Lisandro Kamimura <glk.05@outlook.com>
2023-09-07 16:50:21 -0300

    arreglo

--- src/universidadeje/Entidades/Alumno.java
+++ src/universidadeje/Entidades/Alumno.java
@@ -5,10 +5,92 @@
  */
 package universidadeje.Entidades;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author kamar
  */
 public class Alumno {
+    
+    private int idAlumno;
+    private String apellido;
+    private String nombre;
+    private LocalDate fechaNac;
+    private boolean estado;
+    private int dni;
+   
+    public Alumno(){
+    }
+    public Alumno(int idAlumno, String apellido, String nombre, LocalDate fechaNac, boolean estado, int dni){
+        this.idAlumno=idAlumno;
+        this.apellido=apellido;
+        this.nombre=nombre;
+        this.fechaNac=fechaNac;
+        this.estado=estado;
+        this.dni=dni;
+    }
+
+    public Alumno(String apellido, String nombre, LocalDate fechaNac, boolean estado, int dni) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNac=fechaNac;
+        this.estado=estado;
+        this.dni=dni;    
+    }
+
+    public LocalDate getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(LocalDate fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "idAlumno=" + idAlumno + ", apellido=" + apellido + ", nombre=" + nombre + ", dni="+ dni +'}';
+    }
+    
     
 }
--- src/universidadeje/Entidades/Materia.java
+++ src/universidadeje/Entidades/Materia.java
@@ -5,10 +5,71 @@
  */
 package universidadeje.Entidades;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author kamar
  */
 public class Materia {
+    
+    private int idMateria;
+    private String nombre;
+    private LocalDate anioMateria;
+    private boolean estado;
+    
+    public Materia(){
+    }
+    
+    public Materia(int idMateria, String nombre, LocalDate anioMateria, boolean estado){
+        this.idMateria=idMateria;
+        this.nombre=nombre;
+        this.anioMateria=anioMateria;
+        this.estado=estado;
+    }
+
+    public Materia(String nombre, LocalDate anioMateria, boolean estado) {
+        this.nombre = nombre;
+        this.anioMateria = anioMateria;
+        this.estado = estado;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getAnioMateria() {
+        return anioMateria;
+    }
+
+    public void setAnioMateria(LocalDate anioMateria) {
+        this.anioMateria = anioMateria;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", anioMateria=" + anioMateria + '}';
+    }
+    
     
 }
